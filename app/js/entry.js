66b7c4bf0e9aca742cf8a8bff25d11ab6db67a8d
MARIA ROMERO <mariaromero18e@gmail.com>
2016-05-10 11:39:37 -0700

    create server, gulpfile, app directory

---
+++ app/js/entry.js
@@ -0,0 +1,2 @@
+const angular = require('angular');
+const basicApp = angular.module('basicApp', []);
--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,30 @@
 const gulp = require('gulp');
 const eslint = require('gulp-eslint');
-const mocha = require('gulp-mocha');
+const webpack = require('webpack-stream');
+var allFiles = ['app/**/*.js', 'lib/**/*.js', 'test/**/*test.js', 'gulpfile.js',
+                'index.js', 'server.js'];
+
+gulp.task('lint', () => {
+  return gulp.src(allFiles)
+  .pipe(eslint())
+  .pipe(eslint.format());
+});
+
+gulp.task('webpack_dev', () => {
+  gulp.src('app/js/entry.js')
+    .pipe(webpack( {
+      output: {
+        filename: 'bundle.js'
+      }
+    }))
+    .pipe(gulp.dest('./build'));
+});
+
+gulp.task('static_dev', () => {
+  gulp.src('app/**/*.html')
+    .pipe(gulp.dest('./build'));
+});
+
+gulp.task('build_dev', ['webpack_dev', 'static_dev']);
+
+gulp.task('default', ['build_dev']);
